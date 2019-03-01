f2dda87e1e4d6b347bc08ee7bf8b8a83f47320ea
Abhinandhan2000 <47881100+Abhinandhan2000@users.noreply.github.com>
2019-03-01 12:08:36 +0530

    Add files via upload

---
+++ rps2.py
@@ -0,0 +1,43 @@
+import random
+a=0
+b=0
+l=['rock','paper','scissor']
+d={'r':'rock','p':'paper','s':'scissor'}
+while True:
+	i=input("enter choice rock(r),paper(p),scissor(s) \nq to quit \n")
+	if i=='q':
+		if(a>b):
+			print("Computer wins the game by ",a,"-",b)
+		elif(a==b):
+			print("It's a tie ",a,"-",b)
+		else:
+			print("You win the game by ", b,"-",a)
+		exit()
+	print("your choice ",d[i])
+	c=random.choice(l)
+	print("comp's choice ",c)
+	if(d[i]==c):
+		print('tie')
+		a=a
+		b=b
+	elif d[i]=='rock' and c=='paper':
+		print('comp wins')
+		a=a+1
+	elif d[i]=='scissor' and c=='rock':
+		print('comp wins')
+		a=a+1
+	elif d[i]=='paper' and c=='scissor':
+		print('comp wins')
+		a=a+1
+	elif d[i]=='paper'and c=='rock':
+		print('you win')
+		b=b+1
+	elif d[i]=='rock'and c=='scissor':
+		print('you win')
+		b=b+1
+	elif d[i]=='scissor'and c=='paper':
+		print('you win')
+		b=b+1
+	else:
+		print("invalid input")
+	print ("comp = " ,a ,"\nuser = " ,b)
