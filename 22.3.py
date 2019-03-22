cdab1e4444bffd65c735e50cd6f906816edcea84
Abhinandhan2000 <47881100+Abhinandhan2000@users.noreply.github.com>
2019-03-22 12:33:10 +0530

    Delete 22.3.py

--- 22.3.py
+++
@@ -1,8 +0,0 @@
-
-
-name = input("Enter your name: ")
-branch = input("Enter your branch: ")
-place = input("Enter your place: ")
-print("\t\t\t\tHii Iam\t", name, "\n\t\t\t\t\tIam studying in", branch, "\n\t\t\t\tI live in", place)
-
-
